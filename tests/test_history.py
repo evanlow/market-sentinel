9e354b58647eef513b539c797313383a1e87cd4a
Evan Low <evan.low@illumetechnology.com>
2026-07-23 16:00:46 +0800

    style: normalize history test imports

--- tests/test_history.py
+++ tests/test_history.py
@@ -14,7 +14,6 @@ from market_sentinel.services.history import ResearchHistoryService
 from market_sentinel.services.risk import RiskEngine
 from market_sentinel.services.types import Metric
 
-
 CALM_VALUES = {
     "spy_drawdown_252d_pct": 0.0,
     "spy_vs_50d_pct": 1.0,
